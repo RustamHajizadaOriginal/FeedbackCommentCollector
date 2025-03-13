c9422c38f88baebb728077500a7a39fedf8248af
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-03-13 09:28:27 +0100

    Counter Components1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const textareaEl = document.querySelector(".form__textarea");
 
-textareaEl.addEventListener("click", () => {});
+const inputHandler = () => {
+  //   console.log("test");
+  // Determine max. number of characters
+  const maxNrChars = 150;
+
+  // determin number of characters currently typed
+};
+
+textareaEl.addEventListener("input", inputHandler);
