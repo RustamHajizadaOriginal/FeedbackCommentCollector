02117dce80d2002d4ff10fdc13b3a5a924a23861
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-03-17 08:25:16 +0100

    Counter Component

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// -- COUNTER COMPONENT ---
 const textareaEl = document.querySelector(".form__textarea");
-
+const counterEl = document.querySelector(".counter");
 const inputHandler = () => {
   //   console.log("test");
   // Determine max. number of characters
   const maxNrChars = 150;
 
   // determin number of characters currently typed
+  const nrCharsTyped = textareaEl.value.length;
+
+  // Calculate number of characters left (max. - currently typed)
+  const charsLeft = maxNrChars - nrCharsTyped;
+  // console.log(charsLeft);
+
+  // Show number of characters left
+  counterEl.textContent = charsLeft;
 };
 
 textareaEl.addEventListener("input", inputHandler);
